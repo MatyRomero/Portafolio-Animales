7312cea060e84a22b5282503e5318ff3835b95b1
matias romero <maty@MacBook-Air-de-matias.local>
2023-10-05 17:02:52 -0300

    prueba

--- core/models.py
+++ core/models.py
@@ -92,15 +92,15 @@ def post_save_mascota(sender, instance, created, **kwargs):
         data = json.loads(message["content"].replace("'", '"'))
         print(data, "esta es la data")
         if len(data) != 0:
-            instance.es_animal = data["Es_Animal"]
-            instance.tipo_de_animal = data["Tipo_de_Animal"]
-            instance.color = data["Color"]
             for tag in data["Tags"]:
                 print(data, "LLEGO ACA?")
                 obj, created = Tag.objects.get_or_create(name=tag)
-                obj.save()
                 instance.tags.add(obj)
                 instance.save()
             instance.save()
+            instance.es_animal = data["Es_Animal"]
+            instance.tipo_de_animal = data["Tipo_de_Animal"]
+            instance.color = data["Color"]
+            instance.save()
             print(data, "LLEGO ACA?")
-        
+            
