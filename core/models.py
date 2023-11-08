6462a1c8484be5eb594b9e38e76b51af4ce22978
matias romero <maty@MacBook-Air-de-matias.local>
2023-11-07 22:06:52 -0300

    prueba

--- core/models.py
+++ core/models.py
@@ -124,10 +124,10 @@ def post_save_mascota(sender, instance, created, **kwargs):
             instance.tipo_de_animal = data["Tipo_de_Animal"]
             instance.color = data["Color"]
             print(data, "LLEGO ACA?")
-            for tag in data["Tags"]:
-                print("Procesando tag:", tag)
-                obj, created = Tag.objects.get_or_create(name=tag)
-                instance.tags.add(obj)
-                print("LLEGO ACA?????????")
-                obj.save()
+            tag_objects = []
+            for tag_name in data["Tags"]:
+                tag, created = Tag.objects.get_or_create(name=tag_name)
+                tag_objects.append(tag)
+
+            instance.tags.set(tag_objects)  # Reemplaza todos los tags existentes por los nuevos
             instance.save()
